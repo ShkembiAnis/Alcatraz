5ee4eed7d457b79968c8162308ff58571ae2e8dd
Fady German <fady.german@hotmail.com>
2024-11-09 13:08:45 +0100

    Add Gameboard initialization and showGameboard method to Game class

--- AlcatrazClient/src/main/java/org/example/Game.java
+++ AlcatrazClient/src/main/java/org/example/Game.java
@@ -4,17 +4,33 @@ import java.rmi.RemoteException;
 
 public class Game {
     Lobby lobby;
+    Gameboard gameboard;
 
-    public Game(Lobby lobby){
+    public Game(Lobby lobby) {
         this.lobby = lobby;
+        this.gameboard = new Gameboard();
     }
 
-    public void start(){
+    public void start() {
+        showGameboard();
         int counter = 0;
         lobby.getPlayers().forEach((key, value) -> {
             System.out.println(key + ": " + value.getClientName());
         });
     }
 
+    public void showGameboard() {
+        gameboard.displayBoard();
 
+    }
+
+    // additional methods for moving figures and determining the winner
+    /*
+     * public void moveFigure(int player, int figure, int steps) { // Move figure on
+     * the gameboard }
+     * nextPlayer() { // Switch to next player }
+     * 
+     * 
+     * 
+     */
 }
