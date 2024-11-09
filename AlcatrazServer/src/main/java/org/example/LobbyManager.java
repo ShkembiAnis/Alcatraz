fcdab0be6ae93002f6466bb31d2e216584e64d57
Fady German <fady.german@hotmail.com>
2024-11-09 13:10:09 +0100

    Add removePlayerFromLobby method to LobbyManager for player removal functionality

--- AlcatrazServer/src/main/java/org/example/LobbyManager.java
+++ AlcatrazServer/src/main/java/org/example/LobbyManager.java
@@ -12,19 +12,27 @@ public class LobbyManager {
         this.lobbies = new HashMap<>();
     }
 
-    public void createLobby(long lobbyId,Lobby lobby) {
+    public void createLobby(long lobbyId, Lobby lobby) {
         lobbies.put(lobbyId, lobby);
     }
+
     public void removeLobby(long lobbyId) {
         lobbies.remove(lobbyId);
     }
+
     public Map<Long, Lobby> getLobbies() {
         return lobbies;
     }
-    public Lobby getLobbyById(long lobbyId){
+
+    public Lobby getLobbyById(long lobbyId) {
         return lobbies.get(lobbyId);
     }
-    public Boolean addPlayerToLobby(long lobbyId, Player player){
+
+    public Boolean addPlayerToLobby(long lobbyId, Player player) {
         return lobbies.get(lobbyId).addPlayer(player);
+    }
+
+    public Boolean removePlayerFromLobby(long lobbyId, Player player) {
+        return lobbies.get(lobbyId).removePlayer(player.getClientName());
     }
 }
