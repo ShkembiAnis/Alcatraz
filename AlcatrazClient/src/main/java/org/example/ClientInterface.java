a35fffc8e07fd9d85643108975f903286ebdca3a
Fady German <fady.german@hotmail.com>
2024-11-09 13:08:36 +0100

    Add nextTurn method to ClientInterface and clean up formatting

--- AlcatrazClient/src/main/java/org/example/ClientInterface.java
+++ AlcatrazClient/src/main/java/org/example/ClientInterface.java
@@ -12,7 +12,18 @@ public interface ClientInterface extends Remote {
 
     void notifyStart(String lobbyId, List<ClientInterface> players) throws RemoteException;
 
-    boolean doMove(Player player, String Move)throws RemoteException;
+    boolean doMove(Player player, String Move) throws RemoteException;
 
-    void getMessage(String message) throws  RemoteException;
+    void getMessage(String message) throws RemoteException;
+
+    void nextTurn() throws RemoteException;
+
+    // additonal methods
+    /*
+     * void nextTurn() throws RemoteException; // to notify the next player to start
+     * their turn
+     * extend doMove() to include broadcasting the move to all players // send the
+     * move to all players
+     * 
+     */
 }
