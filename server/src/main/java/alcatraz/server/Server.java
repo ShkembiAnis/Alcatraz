9d032a17c016d72535e5f1fd4750e2c92f9cb5a6
moreno <moreno.marrara@stud.fh-campuswien.ac.at>
2024-11-24 12:00:16 +0100

    implemented locks for the existing functionality

--- server/src/main/java/alcatraz/server/Server.java
+++ server/src/main/java/alcatraz/server/Server.java
@@ -7,11 +7,13 @@ import alcatraz.shared.*;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.*;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Server extends UnicastRemoteObject implements ServerInterface {
 
     private final SharedState state;
     private final ReplicationInterface replication;
+    private final ReentrantLock fairLock = new ReentrantLock();     //MM20241124: simple lock would be enough and may be changed
 
     public Server(ReplicationInterface replication) throws RemoteException {
         super();
@@ -23,6 +25,7 @@ public class Server extends UnicastRemoteObject implements ServerInterface {
     @Override
     public void registerPlayer(String clientName, ClientInterface client) throws RemoteException {
         if (this.replication.isPrimary()) {
+            fairLock.lock();
             if (this.state.players.containsKey(clientName)) {
                 //MM20241121: implement a dedicated exception class
                 throw new RemoteException();
@@ -33,6 +36,7 @@ public class Server extends UnicastRemoteObject implements ServerInterface {
             this.state.players.put(clientName, new Player(client, clientName, "ip_address", "port"));
             System.out.println(clientName + " registered");
             this.replication.replicatePrimaryState();
+            fairLock.unlock();
             // TODO: there should be an early return instead of else
             //MM20241121: why?
         } else {
@@ -44,13 +48,14 @@ public class Server extends UnicastRemoteObject implements ServerInterface {
     @Override
     public LobbyKey createLobby(Player owner) throws RemoteException {
         if (this.replication.isPrimary()) {
+            fairLock.lock();
             //MM20241121: This section needs a mutex
             LobbyKey key = this.state.lobbyManager.createLobby(owner);
             System.out.println("Lobby created with ID: " + key.id + " by Player: " + owner.getClientName());
 
             this.state.lobbyManager.addPlayerToLobby(key.id, owner);
             this.replication.replicatePrimaryState();
-
+            fairLock.unlock();
             return key;
         } else {
             // Forward the request to the primary server
@@ -64,7 +69,7 @@ public class Server extends UnicastRemoteObject implements ServerInterface {
     @Override
     public void joinLobby(Player client, Long lobbyId) throws RemoteException {
         if (this.replication.isPrimary()) {
-            //MM20241121: this section needs a mutex
+            fairLock.lock();
             if (this.state.lobbyManager.getLobbies().containsKey(lobbyId)) {
 
                 this.state.lobbyManager.addPlayerToLobby(lobbyId, this.state.players.get(client.getClientName()));
@@ -72,8 +77,10 @@ public class Server extends UnicastRemoteObject implements ServerInterface {
 
                 // Replication logic: Update backups with the new lobbies state
                 this.replication.replicatePrimaryState();
+                fairLock.unlock();
             }
             else {
+                fairLock.lock();
                 throw new RemoteException();        //MM20241121: find or implement appropriate exception!
             }
         } else {
@@ -93,6 +100,7 @@ public class Server extends UnicastRemoteObject implements ServerInterface {
     @Override
     public void initializeGameStart(long lobbyId) throws RemoteException {
         if (this.replication.isPrimary()) {
+            //MM20241124: do we need a lock here?
             Lobby lobby = this.state.lobbyManager.getLobbyById(lobbyId);
             lobby.getPlayers().forEach((key, value) -> {
                 try {
@@ -119,6 +127,7 @@ public class Server extends UnicastRemoteObject implements ServerInterface {
     // TODO: method not used
     //MM20241124: unnecessary. will remove in next cycle after further checks
     private Boolean checkIfUsernameExists(String playerName) {
+        //MM20241124: do we need a lock here? I would assume as otherwise we get corrupted values
         for (String key : this.state.players.keySet()) {
             if (playerName.equals(key)) {
                 return true;
@@ -136,6 +145,7 @@ public class Server extends UnicastRemoteObject implements ServerInterface {
      * @return true if player is not in a lobby, else false
      */
     private boolean isPlayerInAnyLobby(Map<Long, List<String>> lobbies, String playerId) {
+        //MM20241124: I think this requires a lock as otherwise corrupted values will be returned
         for (List<String> players : lobbies.values()) {
             if (players.contains(playerId)) {
                 return true;
